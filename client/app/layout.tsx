ac2e65702f3bac043ce4a2258b0319277c763504
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-03-16 20:25:35 +0530

    Title changes

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -10,8 +10,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: "StoreIt",
-  description: "StoreIt - The only storage solution you need.",
+  title: "Storify",
+  description: "Storify - The only storage solution you need.",
 };
 
 export default function RootLayout({
