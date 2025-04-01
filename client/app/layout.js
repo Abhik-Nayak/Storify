02047c444bb987d750bfc2b673da427d174bbfc1
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-04-01 06:17:52 +0530

    dashboard setup done

--- client/app/layout.js
+++ client/app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'My Next.js Admin Dashboard',
-  description: 'Next.js App',
+  title: 'Lama Dev Next.js Admin Dashboard',
+  description: 'Next.js Tutorial',
 }
 
 export default function RootLayout({ children }) {
