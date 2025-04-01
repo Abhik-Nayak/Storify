a7eb631bc3e16a98116a28d6af650220f8b89e01
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-04-01 06:53:22 +0530

    user product section dev

--- client/app/dashboard/products/page.jsx
+++ client/app/dashboard/products/page.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Link from "next/link";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a Product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <div className={styles.product}>
+              <Image
+                src={"/noproduct.jpg"}
+                alt=""
+                width={40}
+                height={40}
+                className={styles.productImage}
+              />
+              Title
+            </div>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={`/dashboard/users/`}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <form>
+                  <input type="hidden" name="id" />
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+};
 
-export default page
+export default page;
---
+++ client/app/ui/dashboard/search/search.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import styles from "./search.module.css";
+import { MdSearch } from 'react-icons/md';
+
+const Search = ({ placeholder }) => {
+  return (
+    <div className={styles.container}>
+      <MdSearch />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={styles.input}
+        // onChange={handleSearch}
+      />
+    </div>
+  )
+}
+
+export default Search
